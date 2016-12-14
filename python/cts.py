34e462cf711467d3fe431a30301b19d258b81654
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2016-12-14 21:18:51 +0100

    full_search_via_rss

--- python/cts.py
+++ python/cts.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import json
 import requests
+import xml.etree.ElementTree as ET
+from html import unescape
 
 debug = False
 
@@ -29,4 +31,32 @@ def quick_search_print_subdomains(pattern = ""):
     subdomains = sorted(quick_search(pattern)[0])
     for single in subdomains:
         print(single)
-    return
+    return subdomains
+
+def full_search_via_rss(pattern = ""):
+    certs = []
+    if not debug:
+        req_url = "https://crt.sh/atom?q="+pattern
+        req = requests.get(req_url)
+        if req.status_code != 200:
+            print("! Request for full search via RSS failed.")
+            print("! Possible problems: network issues or too many certs match pattern.")
+            exit(1)
+        req_response = req.text
+    else:
+        with open('rss.xml', 'r') as f:
+            req_response = f.read()
+    req_response = req_response.replace('xmlns="http://www.w3.org/2005/Atom"', '')
+    root = ET.fromstring(req_response)
+
+    for node in root.iter('summary'):
+        node_text = unescape(node.text)
+        div_start_pos = node_text.find("<div")
+        div_start_pos = node_text.find(">", div_start_pos) + 1
+        div_end_pos = node_text.find("</div>")
+        cert_text = node_text[div_start_pos:div_end_pos]
+        cert_text = cert_text.replace("<br>", "\n")
+        certs.append(cert_text)
+    return list(set(certs))
+
+
